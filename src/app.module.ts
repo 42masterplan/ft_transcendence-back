f211c2dfa049ee7ae61333b2cc88c045baa8e7cf
jooocy <88752693+jooocy@users.noreply.github.com>
2023-10-23 23:27:28 +0900

    build: install and add joi (#7)

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './feature/database/database.module';
 import { ApiModule } from './feature/api/api.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
@@ -13,6 +14,15 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath:
         process.env.NODE_ENV === 'develop' ? '.env.develop' : '.env.production',
+
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().default(3306),
+        POSTGRES_USERNAME: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DATABASE: Joi.string().required(),
+      }),
     }),
   ],
   controllers: [AppController],
