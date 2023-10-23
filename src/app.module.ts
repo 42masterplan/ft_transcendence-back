db69945ea7672cba787dc5cf24df61c783928dd6
Jonghyun-Joo <jojo@monymony.co>
2023-10-23 23:15:18 +0900

    build: install and add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './feature/database/database.module';
 import { ApiModule } from './feature/api/api.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule, ApiModule],
+  imports: [
+    DatabaseModule,
+    ApiModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'develop' ? '.env.develop' : '.env.production',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
