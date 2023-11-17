ae50dd8f7321f4cf58d5ff291b2a7809f72a0418
남예지 <yum_yeji@nam-yejiui-MacBookPro.local>
2023-11-17 21:45:45 +0900

    [feat] channel gateway

--- src/feature/gateway/channel/channel.gateway.ts
+++ src/feature/gateway/channel/channel.gateway.ts
@@ -1,20 +1,55 @@
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { Socket } from 'dgram';
+import { CreateChannelDto } from './dto/createChannel.dto';
+import { ChannelService } from './channel.service';
 
-@WebSocketGateway()
-export class ChannelGateway {
-  @SubscribeMessage('message')
-  handleMessage(
-    @MessageBody() data: string,
-    @ConnectedSocket() client: Socket,
-  ): string {
+@WebSocketGateway(4001)
+export class ChannelGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server;
+  constructor(private readonly channelService: ChannelService) {}
+
+  handleConnection(client: any, ...args: any[]) {
+    console.log("It's get connected!");
+  }
+
+  handleDisconnect(client: any) {}
+
+  @SubscribeMessage('newMessage')
+  handleMessage(data: any): string {
     // console.log(client);
-    console.log(data);
+    console.log(data.msg);
     return data;
+  }
+
+  @SubscribeMessage('myChannels')
+  async getMyChannels(client: Socket) {}
+
+  @SubscribeMessage('myRole')
+  async getMyRole(client: Socket, roomId) {
+    console.log(client);
+    this.server.emit('myRole', roomId); // 테이블에 roomId랑 userId검색하기
+  }
+
+  @SubscribeMessage('createChannel')
+  async createChannel(
+    client: Socket,
+    createChannelDto: CreateChannelDto,
+    done,
+  ) {
+    console.log('socket: createChannel');
+    this.channelService.createChannel(client, createChannelDto);
+    // done();
+    client.emit('myChannels');
   }
 }
