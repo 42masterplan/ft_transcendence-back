1196f68882d1de22ccf04e13cb9cab295b43ae33
Jonghyun-Joo <jojo@monymony.co>
2023-11-26 10:32:48 +0900

    chore(users): add to friends controller

--- src/feature/api/users/users.module.ts
+++ src/feature/api/users/users.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersUseCases } from './application/use-case/users.use-case';
 import { UserRepository } from './domain/user.repository';
 import { UserRepositoryImpl } from './infrastructure/user.repository.impl';
+import { FriendsController } from './presentation/controller/friends.controller';
 import { UsersController } from './presentation/controller/users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  controllers: [UsersController],
+  controllers: [UsersController, FriendsController],
   providers: [
     UsersUseCases,
     UsersService,
