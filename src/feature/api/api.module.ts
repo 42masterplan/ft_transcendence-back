b727c24d080e8aaf2b99fe144fb8a5cd2ecf9b62
남예지 <yum_yeji@nam-yejiui-MacBookPro.local>
2023-11-26 22:55:47 +0900

    [feat] add ChannelModule in api Module

--- src/feature/api/api.module.ts
+++ src/feature/api/api.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ChannelModule } from './channels/channel.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, ChannelModule],
 })
 export class ApiModule {}
