85fc4fa7eade08720406e3ee98770539e2ec353d
Jonghyun-Joo <jojo@monymony.co>
2023-12-02 13:18:59 +0900

    chore: remove unused

--- src/feature/api/users/application/friends/find-friends.use-case.ts
+++ src/feature/api/users/application/friends/find-friends.use-case.ts
@@ -19,43 +19,5 @@ export class FindFriendsUseCase {
     );
 
     return await Promise.all(friendPromises);
-
-    // return [
-    //   {
-    //     id: '1',
-    //     profileImage: 'http://localhost:8080/resources/cat_kickBoard.svg',
-    //     name: 'OnlineUser1',
-    //     currentStatus: 'on-line',
-    //     introduction: "Hello, I'm User1. Nice to meet you!"
-    //   },
-    //   {
-    //     id: '2',
-    //     profileImage: 'http://localhost:8080/resources/sloth_health.svg',
-    //     name: 'OfflineUser1',
-    //     currentStatus: 'off-line',
-    //     introduction: "Hello, I'm User2. Nice to meet you!"
-    //   },
-    //   {
-    //     id: '3',
-    //     profileImage: 'http://localhost:8080/resources/crocodile_health.svg',
-    //     name: 'InGameUser3',
-    //     currentStatus: 'in-game',
-    //     introduction: "Hello, I'm User3. Nice to meet you!"
-    //   },
-    //   {
-    //     id: '4',
-    //     profileImage: 'http://localhost:8080/resources/dog_body.svg',
-    //     name: 'OnlineUser4',
-    //     currentStatus: 'on-line',
-    //     introduction: "Hello, I'm User4. Nice to meet you!"
-    //   },
-    //   {
-    //     id: '5',
-    //     profileImage: 'http://localhost:8080/resources/dog_boxing.svg',
-    //     name: 'User5',
-    //     currentStatus: 'off-line',
-    //     introduction: "Hello, I'm User5. Nice to meet you!"
-    //   }
-    // ];
   }
 }
