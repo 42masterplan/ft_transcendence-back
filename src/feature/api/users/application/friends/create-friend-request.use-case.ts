ca3c261634c713598d660ec048d349571648e8e4
jooocy <88752693+jooocy@users.noreply.github.com>
2023-12-02 20:56:46 +0900

    feat(friend-request): implement create (#37)

---
+++ src/feature/api/users/application/friends/create-friend-request.use-case.ts
@@ -0,0 +1,62 @@
+import { FriendRequestRepository } from '../../domain/friend/interface/friend-request.repository';
+import { UserRepository } from '../../domain/user.repository';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+
+@Injectable()
+export class CreateFriendRequestUseCase {
+  private readonly logger = new Logger(CreateFriendRequestUseCase.name);
+
+  constructor(
+    @Inject(FriendRequestRepository)
+    private readonly repository: FriendRequestRepository,
+    @Inject(UserRepository)
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  async execute({
+    primaryUserId,
+    targetUserId,
+  }: {
+    primaryUserId: string;
+    targetUserId: string;
+  }): Promise<void> {
+    if (await this.hasExistingFriendRequest({ primaryUserId, targetUserId })) {
+      return;
+    }
+
+    const targetUser = await this.userRepository.findOneById(targetUserId);
+
+    if (!targetUser) {
+      throw new Error('Target user not found');
+    }
+    this.logger.log(targetUser);
+    return this.repository.save({
+      primaryUserId,
+      targetUserId,
+    });
+  }
+
+  async hasExistingFriendRequest({
+    primaryUserId,
+    targetUserId,
+  }: {
+    primaryUserId: string;
+    targetUserId: string;
+  }): Promise<boolean> {
+    const friendRequests =
+      await this.repository.findManyByPrimaryUserIdTargetUserId({
+        primaryUserId,
+        targetUserId,
+      });
+    this.logger.log(friendRequests);
+    const friendRequest = friendRequests.filter(
+      (friendRequest) => friendRequest.isAcceptedNull,
+    );
+
+    if (friendRequest.length > 0) {
+      return true;
+    }
+
+    return false;
+  }
+}
--- src/feature/api/users/presentation/controller/friends.controller.ts
+++ src/feature/api/users/presentation/controller/friends.controller.ts
@@ -1,3 +1,4 @@
+import { CreateFriendRequestUseCase } from '../../application/friends/create-friend-request.use-case';
 import { FindFriendsUseCase } from '../../application/friends/find-friends.use-case';
 import { FindFriendViewModel } from '../view-models/friends/find-friend.vm';
 import {
@@ -14,7 +15,10 @@ import {
 export class FriendsController {
   private readonly logger = new Logger(FriendsController.name);
 
-  constructor(private readonly findUseCase: FindFriendsUseCase) {}
+  constructor(
+    private readonly findUseCase: FindFriendsUseCase,
+    private readonly createRequestUseCase: CreateFriendRequestUseCase,
+  ) {}
 
   @Get('')
   async getFriends() {
@@ -27,9 +31,16 @@ export class FriendsController {
     return friends.map((friend) => new FindFriendViewModel(friend));
   }
 
-  @Post('')
-  createFriendRequest(@Body('id') id: string) {
-    console.log(id);
+  @Post('/request')
+  async createFriendRequest(@Body('id') id: string) {
+    //TODO: change to user decorator
+    const userId = 'b233ba54-50be-4dcc-9c84-a2ce366936a9';
+
+    await this.createRequestUseCase.execute({
+      primaryUserId: userId,
+      targetUserId: id,
+    });
+
     return true;
   }
 
