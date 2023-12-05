718aded7f188132df68da531c94cc6a40aec5bd5
Hyeonsol Kong <besyia0k0@gmail.com>
2023-12-05 21:46:24 +0900

    Fix: user with unfilled value is not exist user

--- src/feature/api/auth/auth.controller.ts
+++ src/feature/api/auth/auth.controller.ts
@@ -24,6 +24,8 @@ export class AuthController {
       isExist = false;
       const createUserDto = new CreateUserDto(intraId);
       await this.usersService.createOne(createUserDto);
+    } else if (user.name != null || user.email != null) {
+      isExist = false;
     } else {
       is2faEnabled = user.is2faEnabled;
     }
