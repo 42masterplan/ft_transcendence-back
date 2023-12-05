4a88399a853f3ef5da01755f0bccce6e19d23484
남예지 <yum_yeji@nam-yejiui-MacBookPro.local>
2023-12-05 22:13:27 +0900

    [build] isDeleted 추가한거 migration 반영

---
+++ src/migrations/Migration20231205131234.ts
@@ -0,0 +1,21 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20231205131234 extends Migration {
+
+  async up(): Promise<void> {
+    this.addSql('alter table "channel" add column "is_deleted" boolean not null default false;');
+
+    this.addSql('alter table "channel_user_banned" add column "is_deleted" boolean not null default false;');
+
+    this.addSql('alter table "user" add column "is_deleted" boolean not null default false;');
+  }
+
+  async down(): Promise<void> {
+    this.addSql('alter table "channel" drop column "is_deleted";');
+
+    this.addSql('alter table "channel_user_banned" drop column "is_deleted";');
+
+    this.addSql('alter table "user" drop column "is_deleted";');
+  }
+
+}
