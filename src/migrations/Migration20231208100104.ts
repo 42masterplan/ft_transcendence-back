d50e787f43c9f21e76646e18f303a088fbd0b00e
남예지 <yum_yeji@nam-yejiui-MacBookPro.local>
2023-12-09 17:14:26 +0900

    [build] migration update

--- src/migrations/Migration20231208100104.ts
+++ src/migrations/Migration20231208100104.ts
@@ -1,13 +1,13 @@
 import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20231208100104 extends Migration {
-
   async up(): Promise<void> {
-    this.addSql('alter table "channel_participant" add column "is_deleted" boolean not null default false;');
+    this.addSql(
+      'alter table "channel_participant" add column "is_deleted" boolean not null default false;',
+    );
   }
 
   async down(): Promise<void> {
     this.addSql('alter table "channel_participant" drop column "is_deleted";');
   }
-
 }
