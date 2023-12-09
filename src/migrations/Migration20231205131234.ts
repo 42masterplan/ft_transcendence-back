72d79c00a63d85bf4129e3524c1b063d64571b49
남예지 <yum_yeji@nam-yejiui-MacBookPro.local>
2023-12-09 17:32:00 +0900

    [chore] lint migration

--- src/migrations/Migration20231205131234.ts
+++ src/migrations/Migration20231205131234.ts
@@ -1,13 +1,18 @@
 import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20231205131234 extends Migration {
-
   async up(): Promise<void> {
-    this.addSql('alter table "channel" add column "is_deleted" boolean not null default false;');
+    this.addSql(
+      'alter table "channel" add column "is_deleted" boolean not null default false;',
+    );
 
-    this.addSql('alter table "channel_user_banned" add column "is_deleted" boolean not null default false;');
+    this.addSql(
+      'alter table "channel_user_banned" add column "is_deleted" boolean not null default false;',
+    );
 
-    this.addSql('alter table "user" add column "is_deleted" boolean not null default false;');
+    this.addSql(
+      'alter table "user" add column "is_deleted" boolean not null default false;',
+    );
   }
 
   async down(): Promise<void> {
@@ -17,5 +22,4 @@ export class Migration20231205131234 extends Migration {
 
     this.addSql('alter table "user" drop column "is_deleted";');
   }
-
 }
