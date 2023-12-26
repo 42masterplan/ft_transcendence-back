754088288202d1526a5b84bbad5aa164a4d2b68c
Hawardshin <wnddms12345@naver.com>
2023-12-26 23:17:05 +0900

    [build] add MikroOrmModule in NotiModule for EntityRepository

--- src/feature/api/notification/notification.module.ts
+++ src/feature/api/notification/notification.module.ts
@@ -1,10 +1,16 @@
 import { UsersModule } from '../users/users.module';
 import { NotificationUseCases } from './application/notification.use-case';
+import { DmMessageEntity } from './infrastructure/dm-message.entity';
+import { DmEntity } from './infrastructure/dm.entity';
 import { NotificationGateway } from './presentation/notification.gateway';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    MikroOrmModule.forFeature([DmEntity, DmMessageEntity]),
+  ],
   providers: [NotificationGateway, NotificationUseCases],
 })
 export class NotificationModule {}
