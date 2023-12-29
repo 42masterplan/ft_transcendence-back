7dd47c6f297874951154713435c78af27ad49b07
Hyeonsol Kong <besyia0k0@gmail.com>
2023-12-29 15:52:01 +0900

    Refactor: separate to service

--- src/feature/api/game/application/game.service.ts
+++ src/feature/api/game/application/game.service.ts
@@ -1,16 +1,33 @@
+import { Ball } from '../presentation/type/ball';
 import { GameState } from '../presentation/type/game-state';
+import { Player } from '../presentation/type/player';
+import {
+  PADDLE_OFFSET,
+  PLAYER_WIDTH,
+  SCORE_LIMIT,
+  SCREEN_HEIGHT,
+  SCREEN_WIDTH,
+} from '../presentation/util';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class GameService {
-  getMatchId(gameStates: GameState[]): string {
-    return gameStates.length.toString();
+  getNewMatchId(gameStates: GameState[]): string {
+    const matchIndex = gameStates.length - 1;
+    if (!gameStates.length) return '0';
+    if (gameStates[matchIndex].playerB.id === '') return matchIndex.toString();
+    return (matchIndex + 1).toString();
   }
 
-  joinMatch(gameStates: GameState[], matchId: string, clientId: string) {
+  joinMatch(
+    gameStates: GameState[],
+    matchId: string,
+    clientId: string,
+  ): GameState[] {
+    // TODO: game 참가 로직 변경
     let gameState = gameStates.at(-1);
 
-    if (gameState.playerB.id !== '') {
+    if (!gameStates.length || gameState.playerB.id !== '') {
       console.log('create new match');
       gameStates.push(new GameState(matchId));
       gameState = gameStates.at(-1);
@@ -21,7 +38,118 @@ export class GameService {
       gameState.playerB.id = clientId;
       gameState.isReady = true;
     }
+    return gameStates;
   }
 
   startMatch() {}
+
+  getMatch(gameStates: GameState[], clientId: string): GameState {
+    const matchIndex = gameStates.findIndex((state) => {
+      if (state.playerA.id === clientId || state.playerB.id === clientId)
+        return true;
+      return false;
+    });
+    if (matchIndex === -1) console.error('this should not happen'); // 게임 룸을 찾지 못하면 에러를 출력합니다. (로직상 불가능합니다 ;)..
+    return gameStates[matchIndex];
+  }
+
+  getMe(gameState: GameState, clientId: string): Player {
+    return gameState.playerA.id === clientId
+      ? gameState.playerA
+      : gameState.playerB;
+  }
+
+  deleteMatch(gameStates: GameState[], match: GameState) {
+    const matchIndex = gameStates.findIndex((state) => {
+      if (state.matchId === match.matchId) return true;
+      return false;
+    });
+    delete gameStates[matchIndex];
+    gameStates.splice(matchIndex, 1);
+    //TODO: delete 필요?
+  }
+
+  matchForfeit(match: GameState, losePlayerId: string) {
+    if (match.playerA.id === losePlayerId) match.score.playerB = SCORE_LIMIT;
+    else if (match.playerB.id === losePlayerId)
+      match.score.playerA = SCORE_LIMIT;
+    match.isForfeit = true;
+    //TODO: 저장 후 delete
+  }
+
+  movePlayer(player: Player, isA: boolean, keycode: string) {
+    // 키에 따라 플레이어의 위치를 업데이트합니다. 화면 중앙 가까이 한계선을 설정해서 넘어가지 않도록 합니다.
+    // 플레이어가 움직이고 있기 때문에 dx를 업데이트합니다. (스핀 적용을 위해서 필요합니다. dy는 사용하지 않습니다.)
+    switch (keycode) {
+      case 'a': {
+        if (player.x > 0) {
+          player.x -= PADDLE_OFFSET;
+          player.dx = -PADDLE_OFFSET;
+        }
+        break;
+      }
+      case 'd': {
+        if (player.x < SCREEN_WIDTH - player.width) {
+          player.x += PADDLE_OFFSET;
+          player.dx = PADDLE_OFFSET;
+        }
+        break;
+      }
+      case 'w': {
+        if (!isA && player.y > 0) player.y -= PADDLE_OFFSET / 2;
+        else if (isA && player.y > (SCREEN_HEIGHT / 3) * 2 - player.height)
+          player.y -= PADDLE_OFFSET / 2;
+        break;
+      }
+      case 's': {
+        if (!isA && player.y < SCREEN_HEIGHT / 3 - player.height) {
+          player.y += PADDLE_OFFSET / 2;
+        } else if (isA && player.y < SCREEN_HEIGHT - player.height)
+          player.y += PADDLE_OFFSET / 2;
+        break;
+      }
+    }
+  }
+
+  moveBall(ball: Ball): string {
+    ball.x += ball.velocity.x;
+    ball.y += ball.velocity.y;
+
+    // 공이 벽에 부딪히면 반사각을 계산하여 적용합니다.
+    if (ball.x - ball.radius <= 1 || ball.x + ball.radius >= SCREEN_WIDTH - 1)
+      ball.velocity.x *= -1;
+    else if (ball.y < 0) return 'A';
+    else if (ball.y > SCREEN_HEIGHT) return 'B';
+    return '';
+  }
+
+  isGameOver(state: GameState): boolean {
+    if (
+      (!state.isDeuce && state.score.playerA === SCORE_LIMIT) ||
+      state.score.playerB === SCORE_LIMIT ||
+      (state.isDeuce &&
+        Math.abs(state.score.playerA - state.score.playerB) >= 2)
+    )
+      return true;
+    return false;
+  }
+
+  resetBall(ball: Ball) {
+    ball.x = SCREEN_WIDTH / 2;
+    ball.y = SCREEN_HEIGHT / 2;
+    ball.velocity.x = 0;
+    ball.velocity.y = 0;
+  }
+
+  readyBall(ball: Ball, winner: Player) {
+    const x = winner.x + PLAYER_WIDTH / 2;
+    const y = winner.y;
+    const dx = x - ball.x;
+    const dy = y - ball.y;
+    const speed = Math.sqrt(dx * dx + dy * dy);
+    const ret_x = (dx / speed) * ball.speed;
+    const ret_y = (dy / speed) * ball.speed;
+    ball.velocity.x = ret_x;
+    ball.velocity.y = ret_y;
+  }
 }
--- src/feature/api/game/presentation/game.gateway.ts
+++ src/feature/api/game/presentation/game.gateway.ts
@@ -3,12 +3,7 @@ import { GameState } from './type/game-state';
 import {
   DEBOUNCING_TIME,
   GAME_STATE_UPDATE_RATE,
-  PADDLE_OFFSET,
   PLAYER_A_COLOR,
-  PLAYER_WIDTH,
-  SCORE_LIMIT,
-  SCREEN_HEIGHT,
-  SCREEN_WIDTH,
 } from './util';
 import { GameStateViewModel } from './view-model/game-state.vm';
 import { UsePipes } from '@nestjs/common';
@@ -47,145 +42,74 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   handleConnection(client: any, ...args: any[]) {
     console.log('Game is get connected!');
-    const matchId = this.gameService.getMatchId(this.gameStates);
+    const matchId = this.gameService.getNewMatchId(this.gameStates);
     client.join(matchId);
     client.emit('joinedRoom', matchId);
-    // TODO: game 참가 로직 변경
-    if (!this.gameStates[this.currentGameKey]) {
-      // 현재 게임 키에 해당하는 게임 상태가 없으면 새로 생성합니다. (2명 중 1명이 들어온 경우)
-      console.log('create new match');
-      this.gameStates[this.currentGameKey] = new GameState(matchId);
-      this.gameStates[this.currentGameKey].playerA.id = client.id; // 플레이어 A의 소켓 ID를 초기화합니다.
-    } else {
-      // 현재 게임 키에 해당하는 게임 상태가 있으면 플레이어 B의 소켓 ID를 초기화합니다.
-      console.log('you join the match');
-      this.gameStates[this.currentGameKey].playerB.id = client.id; // 플레이어 A의 소켓 ID를 초기화합니다.
-      this.gameStates[this.currentGameKey].isReady = true;
-      this.currentGameKey++; // 다음 게임 키를 위해 게임 키를 1 증가시킵니다.
-    }
+    this.gameService.joinMatch(this.gameStates, matchId, client.id);
   }
 
   handleDisconnect(client: any) {
     console.log('Game is get disconnected!');
-    const matchIndex = this.gameStates.findIndex((state) => {
-      if (state.playerA.id === client.id || state.playerB.id === client.id)
-        return true;
-      return false;
-    });
-    if (matchIndex === -1) console.error('this should not happen'); // 게임 룸을 찾지 못하면 에러를 출력합니다. (로직상 불가능합니다 ;)..
-    if (!this.gameStates[matchIndex].isReady) {
+    const match = this.gameService.getMatch(this.gameStates, client.id);
+    if (!match.isReady) {
       // 1명이 들어왔는데 두 번째 플레이어가 들어오기 전에 연결이 끊긴 경우 게임 상태를 삭제합니다.
       // TODO: 로직 확인
-      delete this.gameStates[matchIndex];
+      this.gameService.deleteMatch(this.gameStates, match);
       return;
     }
     // 플레이어 A가 연결을 끊으면 플레이어 B가 기권승합니다.
-    const state: GameState = this.gameStates[matchIndex];
-    if (state.playerA.id === client.id) {
-      state.score.playerB = SCORE_LIMIT;
-      state.isForfeit = true;
-
-      this.server
-        .to(state.matchId)
-        .emit('updateScore', new GameStateViewModel(state));
-      this.server
-        .to(state.matchId)
-        .emit('gameOver', new GameStateViewModel(state));
-    }
-    // 플레이어 B가 연결을 끊으면 플레이어 A가 기권승합니다.
-    else if (state.playerB.id === client.id) {
-      state.score.playerA = SCORE_LIMIT;
-      state.isForfeit = true;
-      this.server
-        .to(state.matchId)
-        .emit('updateScore', new GameStateViewModel(state));
-      this.server
-        .to(state.matchId)
-        .emit('gameOver', new GameStateViewModel(state));
-    }
+    this.gameService.matchForfeit(match, client.id);
+    this.server
+      .to(match.matchId)
+      .emit('updateScore', new GameStateViewModel(match));
+    this.server
+      .to(match.matchId)
+      .emit('gameOver', new GameStateViewModel(match));
   }
 
   @SubscribeMessage('keyDown')
   playerKeyDown(client: Socket, keycode: string) {
-    const matchIndex = this.gameStates.findIndex((state) => {
-      if (state.playerA.id === client.id || state.playerB.id === client.id)
-        return true;
-      return false;
-    });
-    if (matchIndex === -1) console.error('this should not happen'); // 게임 룸을 찾지 못하면 에러를 출력합니다. (로직상 불가능합니다 ;)..
-    const state = this.gameStates[matchIndex];
-    const targetPlayer =
-      state.playerA.id === client.id ? state.playerA : state.playerB;
+    const match = this.gameService.getMatch(this.gameStates, client.id);
+    const targetPlayer = this.gameService.getMe(match, client.id);
     if (targetPlayer.id === '') return;
     const isA = targetPlayer.color === PLAYER_A_COLOR;
-    // 키에 따라 플레이어의 위치를 업데이트합니다. 화면 중앙 가까이 한계선을 설정해서 넘어가지 않도록 합니다.
-    // 플레이어가 움직이고 있기 때문에 dx를 업데이트합니다. (스핀 적용을 위해서 필요합니다. dy는 사용하지 않습니다.)
-    switch (keycode) {
-      case 'a': {
-        if (targetPlayer.x > 0) {
-          targetPlayer.x -= PADDLE_OFFSET;
-          targetPlayer.dx = -PADDLE_OFFSET;
-        }
-        break;
-      }
-      case 'd': {
-        if (targetPlayer.x < SCREEN_WIDTH - targetPlayer.width) {
-          targetPlayer.x += PADDLE_OFFSET;
-          targetPlayer.dx = PADDLE_OFFSET;
-        }
-        break;
-      }
-      case 'w': {
-        if (!isA && targetPlayer.y > 0) targetPlayer.y -= PADDLE_OFFSET / 2;
-        else if (
-          isA &&
-          targetPlayer.y > (SCREEN_HEIGHT / 3) * 2 - targetPlayer.height
-        )
-          targetPlayer.y -= PADDLE_OFFSET / 2;
-        break;
-      }
-      case 's': {
-        if (!isA && targetPlayer.y < SCREEN_HEIGHT / 3 - targetPlayer.height) {
-          targetPlayer.y += PADDLE_OFFSET / 2;
-        } else if (isA && targetPlayer.y < SCREEN_HEIGHT - targetPlayer.height)
-          targetPlayer.y += PADDLE_OFFSET / 2;
-        break;
-      }
-    }
+    this.gameService.movePlayer(targetPlayer, isA, keycode);
   }
 
   @SubscribeMessage('keyUp')
   playerKeyUp(client: Socket) {
-    console.log('key up');
-    const matchIndex = this.gameStates.findIndex((state) => {
-      if (state.playerA.id === client.id || state.playerB.id === client.id)
-        return true;
-      return false;
-    });
-    if (matchIndex === -1) console.error('this should not happen'); // 게임 룸을 찾지 못하면 에러를 출력합니다. (로직상 불가능합니다 ;)..
-    const state = this.gameStates[matchIndex];
-    const targetPlayer =
-      state.playerA.id === client.id ? state.playerA : state.playerB;
+    const match = this.gameService.getMatch(this.gameStates, client.id);
+    const targetPlayer = this.gameService.getMe(match, client.id);
     if (targetPlayer.id === '') return;
     targetPlayer.dx = 0;
   }
 
   private updateGameState(state: GameState) {
     // 현재 속도에 따라 공의 위치를 업데이트합니다.
-    state.ball.x += state.ball.velocity.x;
-    state.ball.y += state.ball.velocity.y;
+    const winnerStr = this.gameService.moveBall(state.ball);
+    if (winnerStr !== '') {
+      if (winnerStr === 'A') state.score.playerA++;
+      if (winnerStr === 'B') state.score.playerB++;
+      const winner = winnerStr === 'A' ? state.playerA : state.playerB;
+      this.server
+        .to(state.matchId)
+        .emit('updateScore', new GameStateViewModel(state));
 
-    // 공이 벽에 부딪히면 반사각을 계산하여 적용합니다.
-    if (
-      state.ball.x - state.ball.radius <= 1 ||
-      state.ball.x + state.ball.radius >= SCREEN_WIDTH - 1
-    )
-      state.ball.velocity.x *= -1;
-    // 공이 화면 위 아래에 닿으면 점수를 획득하고 공을 초기화합니다.
-    else if (state.ball.y < 0) this.resetBall(true, state); // A 점수 획득
-    else if (state.ball.y > SCREEN_HEIGHT) this.resetBall(false, state); // B 점수 획득
-    // 플레이어와 충돌하면 충돌 시간을 업데이트하고 반사각을 계산하여 적용합니다.
-    // 충돌 시간이 DEBOUNCING_TIME보다 작으면 충돌을 무시합니다. (무한 충돌 이벤트 발생 방지)
+      if (this.gameService.isGameOver(state)) {
+        this.server
+          .to(state.matchId)
+          .emit('gameOver', new GameStateViewModel(state));
+        this.server.socketsLeave(state.matchId);
+      } else {
+        this.gameService.resetBall(state.ball);
+        setTimeout(() => {
+          this.gameService.readyBall(state.ball, winner);
+          this.server
+            .to(state.matchId)
+            .emit('updateBall', new GameStateViewModel(state));
+        }, 3000);
+      }
+    }
     if (
       state.playerA.isCollided(state.ball) ||
       state.playerB.isCollided(state.ball)
@@ -201,58 +125,6 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
       else if (state.playerB.isCollided(state.ball))
         state.playerB.handleCollision(state.ball, now);
     }
-  }
-
-  private resetBall(isA: boolean, state: GameState) {
-    console.log('reset');
-    if (isA) state.score.playerA++;
-    else state.score.playerB++;
-    this.server
-      .to(state.matchId)
-      .emit('updateScore', new GameStateViewModel(state));
-    if (
-      (!state.isDeuce && state.score.playerA === SCORE_LIMIT) ||
-      state.score.playerB === SCORE_LIMIT
-    ) {
-      this.server
-        .to(state.matchId)
-        .emit('gameOver', new GameStateViewModel(state));
-      this.server.socketsLeave(state.matchId);
-      return;
-    } else if (
-      state.isDeuce &&
-      Math.abs(state.score.playerA - state.score.playerB) >= 2
-    ) {
-      this.server
-        .to(state.matchId)
-        .emit('gameOver', new GameStateViewModel(state));
-      this.server.socketsLeave(state.matchId);
-      return;
-    }
-    state.ball.x = SCREEN_WIDTH / 2;
-    state.ball.y = SCREEN_HEIGHT / 2;
-    state.ball.velocity.x = 0;
-    state.ball.velocity.y = 0;
-    console.log('reset fin');
-    console.log(state.ball);
-    setTimeout(() => {
-      const x = !isA
-        ? state.playerA.x + PLAYER_WIDTH / 2
-        : state.playerB.x + PLAYER_WIDTH / 2;
-      const y = !isA ? state.playerA.y : state.playerB.y;
-      const dx = x - state.ball.x;
-      const dy = y - state.ball.y;
-      const speed = Math.sqrt(dx * dx + dy * dy);
-      const ret_x = (dx / speed) * state.ball.speed;
-      const ret_y = (dy / speed) * state.ball.speed;
-      state.ball.velocity.x = ret_x;
-      state.ball.velocity.y = ret_y;
-      console.log('game re-start');
-      console.log(state.ball);
-      this.server
-        .to(state.matchId)
-        .emit('updateBall', new GameStateViewModel(state));
-    }, 3000);
   }
 
   updateGameStateCron() {
@@ -276,7 +148,7 @@ export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
 
   updateGameTimeCron() {
     console.log('start update game time cron');
-    const timerId = setTimeout(() => {
+    const timerId = setInterval(() => {
       this.gameStates.forEach((state) => {
         state.time--;
         if (!state.isReady) return;
