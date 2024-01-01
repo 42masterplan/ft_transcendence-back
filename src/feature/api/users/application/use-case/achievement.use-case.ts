823b958fba3038abf72fdb70074a3c525404b260
남예지 <yum_yeji@nam-yejiui-MacBookPro.local>
2024-01-01 22:53:31 +0900

    [feat] feature useCase files

---
+++ src/feature/api/users/application/use-case/achievement.use-case.ts
@@ -0,0 +1,108 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { AchievementRepository } from "../../domain/achievement.repository";
+import { AchievementStatusRepository } from "../../domain/achievement-status.repository";
+
+@Injectable()
+export class AchievementUseCase {
+  constructor(
+    @Inject(AchievementRepository)
+    private readonly achievementRepository: AchievementRepository,
+    @Inject(AchievementStatusRepository)
+    private readonly achievementStatusRepository: AchievementStatusRepository
+  ) {}
+
+  private names: string[] = ["너진짜잘한다", "너진짜못한다", "와우! 첫 승리!", "이런! 첫 패배!", "힘의 차이가 느껴지십니까?", "스토커", "여기여기 모여라", "쉿! 조용히 해!", "꼴보기 싫어", "잠깐 나가줄래?"];
+  private descriptions: string[] = ["게임으로 3연승을 거두세요.", "게임으로 3연패를 당하세요.", "게임으로 첫 승리를 거두세요.", "게임으로 첫 패배를 당하세요.", "두배 점수차로 승리하세요", "연속으로 10개의 메세지를 보내세요", "채팅창을 만들어 보세요!", "참가자를 뮤트해 보세요", "참가자를 밴해 보세요", "참가자를 킥해 보세요"];
+
+    async findAllByUserId(userId: string) {
+      const achievementStatuses = await this.achievementStatusRepository.findAllByUserId(userId);
+      console.log("achievement", achievementStatuses);
+
+      return achievementStatuses.map(async (achievementStatus) => {
+        const achievement = await this.achievementRepository.findOneById(achievementStatus.achievementId);
+        return {
+          name: this.names[achievement.id],
+          description: this.descriptions[achievement.id],
+          progressRate: achievementStatus.count / achievement.criterionNumber * 100,
+        };
+      });
+    }
+
+    async initAchievementStatus(userId: string) {
+      const achievements = await this.achievementRepository.findAll();
+      for (const achievement of achievements) {
+        await this.achievementStatusRepository.saveOne(userId, achievement.id);
+      }
+    }
+
+    async handleGameAchievement(userId: string, isWin: boolean, score: number) {
+      if (isWin)  {
+        await this.handleFirstWin(userId);
+        await this.handleThreeWins(userId);
+        await this.resetThreeLoses(userId);
+      }
+      else {
+        await this.handleFirstLose(userId);
+        await this.handleThreeLoses(userId);
+        await this.resetThreeWins(userId);
+      }
+    }
+
+    async handleFirstWin(userId: string) {
+      const achievementStatus = await this.achievementStatusRepository.findOneByUserIdAndAchievementId(userId, 3);
+      if (achievementStatus.isAchieved) return;
+      achievementStatus.updateCount(achievementStatus.count + 1);
+      const achievement = await this.achievementRepository.findOneById(3);
+      if (achievementStatus.count >= achievement.criterionNumber) {
+        achievementStatus.updateIsAchieved(true);
+      }
+      await this.achievementStatusRepository.updateOne(achievementStatus);
+    }
+
+    async handleThreeWins(userId: string) {
+      const achievementStatus = await this.achievementStatusRepository.findOneByUserIdAndAchievementId(userId, 1);
+      if (achievementStatus.isAchieved) return;
+      achievementStatus.updateCount(achievementStatus.count + 1);
+      const achievement = await this.achievementRepository.findOneById(1);
+      if (achievementStatus.count >= achievement.criterionNumber) {
+        achievementStatus.updateIsAchieved(true);
+      }
+      await this.achievementStatusRepository.updateOne(achievementStatus);
+    }
+
+    async resetThreeWins(userId: string) {
+      const achievementStatus = await this.achievementStatusRepository.findOneByUserIdAndAchievementId(userId, 1);
+      if (achievementStatus.isAchieved) return;
+      achievementStatus.updateCount(0);
+      await this.achievementStatusRepository.updateOne(achievementStatus);
+    }
+
+    async handleFirstLose(userId: string) {
+      const achievementStatus = await this.achievementStatusRepository.findOneByUserIdAndAchievementId(userId, 4);
+      if (achievementStatus.isAchieved) return;
+      achievementStatus.updateCount(achievementStatus.count + 1);
+      const achievement = await this.achievementRepository.findOneById(4);
+      if (achievementStatus.count >= achievement.criterionNumber) {
+        achievementStatus.updateIsAchieved(true);
+      }
+      await this.achievementStatusRepository.updateOne(achievementStatus);
+    }
+
+    async handleThreeLoses(userId: string) {
+      const achievementStatus = await this.achievementStatusRepository.findOneByUserIdAndAchievementId(userId, 2);
+      if (achievementStatus.isAchieved) return;
+      achievementStatus.updateCount(achievementStatus.count + 1);
+      const achievement = await this.achievementRepository.findOneById(2);
+      if (achievementStatus.count >= achievement.criterionNumber) {
+        achievementStatus.updateIsAchieved(true);
+      }
+      await this.achievementStatusRepository.updateOne(achievementStatus);
+    }
+
+    async resetThreeLoses(userId: string) {
+      const achievementStatus = await this.achievementStatusRepository.findOneByUserIdAndAchievementId(userId, 2);
+      if (achievementStatus.isAchieved) return;
+      achievementStatus.updateCount(0);
+      await this.achievementStatusRepository.updateOne(achievementStatus);
+    }
+}
